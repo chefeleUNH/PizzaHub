419d5721262d001c0922f2a6a68b51fd59e999d3
chefeleUNH <chefele@newhaven.edu>
2020-10-07 09:44:18 -0400

    starter code for getting the pizzerias list from firebase

--- app/src/main/java/edu/newhaven/pizzahub/MainActivity.kt
+++ app/src/main/java/edu/newhaven/pizzahub/MainActivity.kt
@@ -2,10 +2,30 @@ package edu.newhaven.pizzahub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = javaClass.name
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get the list of pizzerias from Firebase
+        db.collection("pizzerias")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents: ", exception)
+            }
     }
+
 }
